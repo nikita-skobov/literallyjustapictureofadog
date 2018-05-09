02bc716f7018f3941fb9a3eda0e8f06ffbc47af7
Ubuntu <ubuntu@ip-172-31-21-199.us-east-2.compute.internal>
2018-05-09 17:38:37 +0000

    passes in the addresses as string variables to be rendered into dog.pug

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,10 @@
 module.exports = (app, port) => {
+
+  //setup variables to be dynamically rendered into the dog.pug template
+  var dog_url  = 'https://d1ahv7jqi833s4.cloudfront.net/dog.png'
+  var my_email = 'nskobov.websites@gmail.com'
+  var github   = 'https://github.com/nikita-skobov/literallyjustapictureofadog'
+
   //literally only need one route for this website
   app.get('/', (req, res) => {
     var real_ip = req.headers['x-real-ip'] || req.connection.remoteAddress
@@ -7,7 +13,8 @@ module.exports = (app, port) => {
     //get the clients ip, and parsed user agent from nginx forwarded headers.
     //This is useful to see if a particular IP is spamming my server.
 
-    res.render('dog') //dog.pug is the filename within the views folder.
+    res.render('dog', {url: dog_url, my_email: my_email, github: github})
+    //dog.pug is the filename within the views folder.
   })
 
 }
